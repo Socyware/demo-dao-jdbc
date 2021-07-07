0e170eaba449ceccdb1e59812ff58ea2e108f6c6
Alan Souza <socyware@gmail.com>
2021-07-07 00:59:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,26 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-
-		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		Locale.setDefault(Locale.US);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		Department obj = new Department(1,"Books");
+		
+		Seller seller = new Seller(21, "Bob ", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(obj);
+		System.out.println();
+		
+		System.out.println(seller);
 		
 	}
 
